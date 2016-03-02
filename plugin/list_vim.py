57f94dbb2e90ad235297f1c7fff302a2127548eb
Michael Johns <mjohns@mjohns.nyc.corp.google.com>
2016-03-02 17:33:10 -0500

    renaming

--- plugin/list_vim.py
+++ plugin/list_vim.py
@@ -4,23 +4,23 @@ import commands
 
 def RunCmd(name, autoOpen = False):
   s,output=commands.getstatusoutput(name)
-  files=output.split('\n')
-  ListFiles(files, autoOpen)
+  lines=output.split('\n')
+  ListOutput(lines, autoOpen)
 
 
-def ListFiles(files, autoOpen):
-  if len(files) == 0 or (len(files)==1 and files[0] == ""):
+def ListOutput(lines, autoOpen):
+  if len(lines) == 0 or (len(lines)==1 and lines[0] == ""):
     vim.command('echohl ErrorMsg')
-    vim.command('echomsg "No files found"')
+    vim.command('echomsg "No output"')
     vim.command('echohl None')
-  elif len(files) == 1 and autoOpen:
-    vim.command('e ' + files[0])
+  elif len(lines) == 1 and autoOpen:
+    vim.command('e ' + lines[0])
   else:
     vim.command('call s:CreateTempBuffer()')
     vim.command('setlocal modifiable')
     vim.command('map <buffer> <Enter> gF')
     vim.command('map <buffer> q <C-^>')
     vim.command('map <buffer> :q q')
-    vim.current.buffer[:] = files
+    vim.current.buffer[:] = lines
     vim.command('setlocal nomodifiable')
     vim.command('0')
